d934312274a467d327a6da7409ce7fe49ef509d4
Bhuwan <pandeybhuwan138@gmail.com>
2023-11-12 22:47:11 +0545

    NgSwitch and NgFor directives

--- 06. Angular/myAngularWorkspace/projects/directive-demo/src/app/app.module.ts
+++ 06. Angular/myAngularWorkspace/projects/directive-demo/src/app/app.module.ts
@@ -5,18 +5,20 @@ import { AppComponent } from './app.component';
 import { IfDemoComponent } from './if-demo/if-demo.component';
 import { FormsModule } from '@angular/forms';
 import { IfElseComponent } from './if-else/if-else.component';
+import { SwitchDemoComponent } from './switch-demo/switch-demo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IfDemoComponent,
-    IfElseComponent
+    IfElseComponent,
+    SwitchDemoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
   providers: [],
-  bootstrap: [IfElseComponent]
+  bootstrap: [SwitchDemoComponent]
 })
 export class AppModule { }
